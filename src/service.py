d15f565dbd905663413540b3ff05489acd3847dc
LUH <unghee66@gmail.com>
2022-10-25 20:41:47 +0900

    Delete rebundancy

--- src/service.py
+++
@@ -1,7 +0,0 @@
-from core import Index, Search, Rank
-
-
-def search(query, algorithm):
-    tokens = Index.preprocess(query)
-    slugs = Search().naive(tokens)
-    return Rank(slugs).work()
--- src/utils.py
+++ src/utils.py
@@ -7,11 +7,3 @@ def printer(step):
             return result
         return wrapper
     return decorator
-
-
-def jaccard_similarity(list1, list2):
-    intersection = len(list(set(list1).intersection(list2)))
-    union = (len(set(list1)) + len(set(list2))) - intersection
-    if union == 0:
-        return 0
-    return float(intersection) / union
